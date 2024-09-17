9ed2deb99735c0ac1730c3256695c0b928bab1e2
Dellius Alexander <dellius.alexander@gmail.com>
2024-09-17 16:00:45 -0400

    added spacing

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -36,6 +36,6 @@ def create_vertical_data(samples, classes):
     y = np.zeros(samples*classes, dtype='uint8')
     for class_number in range(classes):
         ix = range(samples*class_number, samples*(class_number+1))
-        X[ix] = np.c_[np.random.randn(samples)*0.1 + (class_number)/3, np.random.randn(samples)*0.1 + 0.5]
+        X[ix] = np.c_[np.random.randn(samples) * 0.1 + class_number / 3, np.random.randn(samples) * 0.1 + 0.5]
         y[ix] = class_number
     return X, y
